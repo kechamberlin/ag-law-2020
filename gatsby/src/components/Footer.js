a7639e0a43749a0830fba1221306951de41a21cc
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-14 13:34:44 -0700

    Created Footer component with basic starter code.

---
+++ gatsby/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright &copy; {new Date().getFullYear()} AG Law Modesto and Stock -
+        All Rights Reserved
+      </p>
+    </footer>
+  );
+}
