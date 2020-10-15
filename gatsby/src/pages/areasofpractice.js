569e5a6c0c87f0ab782d243ff1b4586d5e88fd5b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-15 13:42:22 -0700

    Removed Layout, Nav components and imports, and replaced Layout with react fragment.

--- gatsby/src/pages/areasofpractice.js
+++ gatsby/src/pages/areasofpractice.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 
 export default function AreasOfPracticePage() {
   return (
-    <Layout>
-      <Nav />
+    <>
       <p>Areas of Practice Page</p>
-    </Layout>
+    </>
   );
 }
