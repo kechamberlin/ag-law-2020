e831c5585aed6d10b92a20f744ff33d433383391
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-15 13:41:39 -0700

    Removed Layout component and import, and replaced it with react fragment.  Also added paragraph tag with 'Another Element'.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
-    <Layout>
+    <>
       <p>Home Page</p>
-    </Layout>
+      <p>Another element</p>
+    </>
   );
 }
