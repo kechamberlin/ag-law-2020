0c404b57717a2b5aa838b34c25bedf6eec1cc7e5
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-15 13:14:06 -0700

    Removed Nav component and import because Layout already has it.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
     <Layout>
-      <Nav />
       <p>Home Page</p>
     </Layout>
   );
