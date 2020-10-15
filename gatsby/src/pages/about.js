1535da0dc036fcb35fbf7040cdd74dea4f78e78a
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-15 13:39:21 -0700

    Removed Nav component and import, and changed div tag to react fragment.

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function AboutPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>About Page</p>
-    </div>
+    </>
   );
 }
