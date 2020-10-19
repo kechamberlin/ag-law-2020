4f733faf00efc70b6d26b0ad98eb0a217c24c4c4
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-19 16:14:25 -0700

    Created a practice.js schema.

---
+++ sanity/schemas/practice.js
@@ -0,0 +1,55 @@
+import { MdGavel as icon } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'practice',
+  // Visible Title
+  title: 'Areas of Practice',
+  type: 'document',
+  icon: icon,
+  fields: [
+    {
+      name: 'practice',
+      title: 'Area of Practice',
+      type: 'string',
+      description: 'Name of the area of practice',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'practice',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'overview',
+      title: 'Overview',
+      type: 'string',
+      description: 'Overview of Practice',
+    },
+    {
+      name: 'categories',
+      title: 'Sub Categories in Practice',
+      type: 'string',
+      description: 'Fields included in this Area',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'practice',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name}`,
+    }),
+  },
+};
