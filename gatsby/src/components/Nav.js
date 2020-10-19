80ed56eed93a02a769e999951085e2d3a9f4f4b5
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-19 16:09:01 -0700

    Imported styled from styled-components, Logo component, and created NavStyles component.

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    list-style: none;
+    display: grid;
+    grid-template-columns: auto 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+    margin-top: -4rem;
+  }
+  a {
+    font-size: 2.4rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--orange);
+    }
+    &[aria-current='page'] {
+      color: var(--orange);
+    }
+  }
+`;
 
 export default function Nav() {
   return (
