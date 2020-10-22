570d75f5b9d371c5ff9193d13e44c158c1f7daaa
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-22 16:27:36 -0700

    Destructured data.

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import PersonList from '../components/PersonList';
 
-export default function AboutPage() {
+export default function AboutPage({ data }) {
   return (
     <>
       <p>About Page</p>
