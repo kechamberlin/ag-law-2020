e2ce9289fa021cd19d2b1f95a9511c08e392a39b
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-22 16:46:26 -0700

    Created RecentCasesGrid, RecentCasesStyles css variables.

--- gatsby/src/pages/recentcases.js
+++ gatsby/src/pages/recentcases.js
@@ -3,6 +3,18 @@ import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
 
+const RecentCasesGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+const RecentCasesStyles = styled.div`
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+`;
+
 export default function RecentCasesPage({ data }) {
   const clients = data.allSanityRecent.nodes;
   return (
