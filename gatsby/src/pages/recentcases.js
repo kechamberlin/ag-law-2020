797df0c4fe00206632140c3af0dc2b8ccdfd7aec
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-10-22 16:44:22 -0700

    Imported graphql, Img component, and styled-components.

--- gatsby/src/pages/recentcases.js
+++ gatsby/src/pages/recentcases.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
 export default function RecentCasesPage() {
   return (
