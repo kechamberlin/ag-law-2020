f1bbbcb60c233205cc9d35b6b04b539ed04abc90
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-11-21 13:47:19 -0800

    Imported SEO component and put at top of return statement.

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PersonList from '../components/PersonList';
+import SEO from '../components/SEO';
 
 export default function AboutPage({ data }) {
   const persons = data.persons.nodes;
   return (
     <>
+      <SEO title="About Anthony" />
       <PersonList persons={persons} />
     </>
   );
