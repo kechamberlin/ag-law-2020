ea8c6e270c8581fad922ced8f90d66bfcf389a3f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-11-21 13:51:15 -0800

    Replaced p tag with a contact form wrapped in SEO and OrderStyles component.

--- gatsby/src/pages/contact.js
+++ gatsby/src/pages/contact.js
@@ -14,7 +14,50 @@ export default function ContactPage() {
 
   return (
     <>
-      <p>Contact Page</p>
+      <SEO title="Contact Anthony" />
+      <OrderStyles>
+        <h2>Contact AG Law</h2>
+        <fieldset>
+          <legend>Your Info</legend>
+
+          <label htmlFor="name">Full Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            value={values.phone}
+            onChange={updateValue}
+          />
+
+          <label htmlFor="message">Tell Us What Happened</label>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="8"
+            value={values.message}
+            onChange={updateValue}
+          />
+        </fieldset>
+      </OrderStyles>
     </>
   );
 }
