67e67493ba71db3a1aeb74f9d50877af4a418f69
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-11-21 13:51:50 -0800

    Imported SEO, destructured title, and put SEO at top of return statement.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
-export default function HomePage() {
+export default function HomePage({ title }) {
   return (
     <>
+      <SEO title="Home" />
       <p>Home Page</p>
       <p>Another element</p>
     </>
