2df65d8d73b9df185b42137b6e6e92dd02268d24
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-11-21 13:49:08 -0800

    Imported useState hook, and SEO, OrderStyles and useForm components.

--- gatsby/src/pages/contact.js
+++ gatsby/src/pages/contact.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SEO from '../components/SEO';
+import OrderStyles from '../styles/OrderStyles';
+import useForm from '../utils/useForm';
 
 export default function ContactPage() {
   return (
